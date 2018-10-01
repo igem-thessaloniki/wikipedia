305c2991cde2ab77cf4929cf9b05f1cafda21c63
Panagiotis Karatakis <karatakis@csd.auth.gr>
2018-10-02 01:24:28 +0300

    Re-arange meny

--- development/public/components/igem-layout.vue
+++ development/public/components/igem-layout.vue
@@ -24,59 +24,59 @@
                 <route-item page="index" link="http://2018.igem.org/Team:Thessaloniki" icon="home" label="Home" sublabel="Our Project homepage">
                 </route-item>
 
-                <q-collapsible icon="people_outline" label="Team" :opened="['team', 'collaborations'].includes(page)">
-                    <route-item page="team" link="http://2018.igem.org/Team:Thessaloniki/Team" icon="people" label="Team Members">
-                    </route-item>
-
-                    <route-item page="collaborations" link="http://2018.igem.org/Team:Thessaloniki/Collaborations" icon="account_balance"
-                        label="Team Collaborations">
-                    </route-item>
-                </q-collapsible>
-
-                <q-collapsible icon="build" label="Project" :opened="['description', 'design', 'experiments', 'notebook', 'interlab', 'model', 'results', 'demonstrate', 'improve', 'attributions',].includes(page)">
-                    <route-item page="description" link="http://2018.igem.org/Team:Thessaloniki/Description" icon="info"
-                        label="Description">
+                <q-collapsible icon="build" label="Project" :opened="['description', 'design', 'results', 'demonstrate', 'safety', 'notebook', 'improve'].includes(page)">
+                    <route-item page="description" link="http://2018.igem.org/Team:Thessaloniki/Description" icon="info" label="Description">
                     </route-item>
                     <route-item page="design" link="http://2018.igem.org/Team:Thessaloniki/Design" icon="create" label="Design">
                     </route-item>
-                    <route-item page="experiments" link="http://2018.igem.org/Team:Thessaloniki/Experiments" icon="colorize"
-                        label="Experiments">
-                    </route-item>
-                    <route-item page="notebook" link="http://2018.igem.org/Team:Thessaloniki/Notebook" icon="notes" label="Notebook">
-                    </route-item>
-                    <route-item page="interlab" link="http://2018.igem.org/Team:Thessaloniki/InterLab" icon="sync" label="InterLab">
-                    </route-item>
-                    <route-item page="model" link="http://2018.igem.org/Team:Thessaloniki/Model" icon="poll" label="Model">
-                    </route-item>
                     <route-item page="results" link="http://2018.igem.org/Team:Thessaloniki/Results" icon="show_charts" label="Results">
                     </route-item>
-                    <route-item page="demonstrate" link="http://2018.igem.org/Team:Thessaloniki/Collaborations" icon="looks"
-                        label="Demonstrate">
+                    <route-item page="demonstrate" link="http://2018.igem.org/Team:Thessaloniki/Demonstrate" icon="looks" label="Demonstrate">
                     </route-item>
+                    <route-item page="safety" link="http://2018.igem.org/Team:Thessaloniki/Safety" icon="verified_user" label="Safety">
+                    </route-item>
+                    
+                    <route-item page="notebook" link="http://2018.igem.org/Team:Thessaloniki/Notebook" icon="notes" label="Notebook">
+                    </route-item>
+                    
                     <route-item page="improve" link="http://2018.igem.org/Team:Thessaloniki/Improve" icon="plus_one" label="Improve">
                     </route-item>
-                    <route-item page="attributions" link="http://2018.igem.org/Team:Thessaloniki/Attributions" icon="people"
-                        label="Attributions">
+                </q-collapsible>
+
+                <q-collapsible icon="waves" label="Wet Lab" :opened="['experiments', 'protocols', 'cloning-strategy', 'interlab'].includes(page)">
+                    <route-item page="experiments" link="http://2018.igem.org/Team:Thessaloniki/Experiments" icon="colorize" label="Experiments">
+                    </route-item>
+                    <route-item page="protocols" link="http://2018.igem.org/Team:Thessaloniki/Protocols" icon="assignment" label="Protocols">
+                    </route-item>
+                    <route-item page="cloning-strategy" link="http://2018.igem.org/Team:Thessaloniki/Cloning_Strategy" icon="flip_to_front" label="Cloning Strategy">
+                    </route-item>
+                    <route-item page="interlab" link="http://2018.igem.org/Team:Thessaloniki/InterLab" icon="sync" label="InterLab">
                     </route-item>
                 </q-collapsible>
 
                 <q-collapsible icon="extensions" label="Parts" :opened="['overview', 'basic', 'composite', 'collection'].includes(page)">
                     <route-item page="overview" link="http://2018.igem.org/Team:Thessaloniki/Parts" icon="info" label="Parts Overview">
                     </route-item>
+                    <route-item page="collection" link="http://2018.igem.org/Team:Thessaloniki/Part_Collection" icon="collections"
+                        label="Part Collection">
+                    </route-item>
                     <route-item page="basic" link="http://2018.igem.org/Team:Thessaloniki/Basic_Part" icon="apps" label="Basic Parts">
                     </route-item>
                     <route-item page="composite" link="http://2018.igem.org/Team:Thessaloniki/Composite_Part" icon="grid_on"
                         label="Composite Parts">
                     </route-item>
-                    <route-item page="collection" link="http://2018.igem.org/Team:Thessaloniki/Part_Collection" icon="collections"
-                        label="Part Collection">
+                </q-collapsible>
+
+                <q-collapsible icon="memory" label="Dry Lab" :opened="['model'].includes(page)">
+                    <route-item page="model" link="http://2018.igem.org/Team:Thessaloniki/Model" icon="poll" label="Model">
                     </route-item>
                 </q-collapsible>
 
-                <route-item page="safety" link="http://2018.igem.org/Team:Thessaloniki/Safety" icon="verified_user" label="Safety">
-                </route-item>
+                <q-collapsible icon="people" label="Human Practices" :opened="['synapsis', 'human-practices', 'public-engagement'].includes(page)">
+                    <route-item page="synapsis" link="http://2018.igem.org/Team:Thessaloniki/Synapsis" icon="settings_input_hdmi"
+                        label="Synapsis">
+                    </route-item>
 
-                <q-collapsible icon="people" label="Human Practices" :opened="['human-practices', 'public-engagement'].includes(page)">
                     <route-item page="human-practices" link="http://2018.igem.org/Team:Thessaloniki/Human_Practices" icon="people_outline"
                         label="Human Practices">
                     </route-item>
@@ -109,6 +109,19 @@
                     </route-item>
 
                     <route-item page="Software" link="http://2018.igem.org/Team:Thessaloniki/Software" icon="code" label="Software">
+                    </route-item>
+                </q-collapsible>
+
+                <q-collapsible icon="people_outline" label="Team" :opened="['team', 'collaborations', 'attributions'].includes(page)">
+                    <route-item page="team" link="http://2018.igem.org/Team:Thessaloniki/Team" icon="people" label="Team Members">
+                    </route-item>
+
+                    <route-item page="collaborations" link="http://2018.igem.org/Team:Thessaloniki/Collaborations" icon="account_balance"
+                        label="Team Collaborations">
+                    </route-item>
+
+                    <route-item page="people_outline" link="http://2018.igem.org/Team:Thessaloniki/Attributions" icon="people"
+                        label="Attributions">
                     </route-item>
                 </q-collapsible>
 
